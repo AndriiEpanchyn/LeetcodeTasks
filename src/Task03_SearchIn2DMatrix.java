58a31b65555a128b447bbd0f07119fbbf51dc90a
epanchinaa19@gmail.com <epanchinaa19@gmail.com>
2022-06-08 11:48:59 +0300

    3 task is done

--- src/Task03_SearchIn2DMatrix.java
+++ src/Task03_SearchIn2DMatrix.java
@@ -13,41 +13,39 @@ import java.util.Arrays;
  */
 public class Task03_SearchIn2DMatrix {
 	private static int[][] testMatrix1 = new int[][] { 
-		{  1,  3, 5, 7, 9 }, 
-		{ 10, 12, 14, 16, 20 }, 
+		{ 1, 3, 5, 7, 9 }, 
+		{ 10, 12, 14, 16, 20 },
 		{ 21, 23, 27, 29, 30 }, 
 		{ 32, 34, 36, 38, 40 }, 
 		{ 41, 43, 45, 47, 49 }, 
-		{ 50, 52, 54, 56, 58 }, 
+		{ 50, 52, 54, 56, 58 },
 		{ 63, 67, 75, 81, 95 } };
-	private static int target1 = 40;
+	private static int target1 = 5;
 
 	public static void run() {
 		boolean b1 = searchMatrix(testMatrix1, target1);
 	}
 
 	private static boolean searchMatrix(int[][] source, int target) {
-		System.out.println("Start proceed matrix. target = " + target );//+ "; Matrix is: " + Arrays.deepToString(source));
-		System.out.println("===========================================");
 		int found;
-
-		int[] zeroColumn = new int[source.length];
-		for (int i = 0; i < source.length; i++) {
-			zeroColumn[i] = source[i][0];
-		}
-
-		int targetRow = findRow(zeroColumn, target);
+		int targetRow = findRow(source, target);
 		if (targetRow == -1) {
 			found = -1;
 		} else {
 			found = Arrays.binarySearch(source[targetRow], target);
 		}
-		System.out.println("return " + (found >= 0));
+		 System.out.println("return " + (found >= 0));
 		return found >= 0;
 	}
 
-	private static int findRow(int[] zeroColumn, int target) {
-		System.out.println(Arrays.toString(zeroColumn));
+	private static int findRow(int[][] source, int target) {
+		int[] zeroColumn = new int[source.length];
+		int[] lastColumn = new int[source.length];
+		for (int i = 0; i < source.length; i++) {
+			zeroColumn[i] = source[i][0];
+			lastColumn[i] = source[i][source[0].length - 1];
+		}
+
 		int startIndex = 0;
 		int endIndex = zeroColumn.length - 1;
 		int i = 0;
@@ -57,19 +55,18 @@ public class Task03_SearchIn2DMatrix {
 		} else if (target > zeroColumn[endIndex]) {
 			return endIndex;
 		} else {
-			while(startIndex<=endIndex) {
+			while (startIndex <= endIndex) {
 				i = (startIndex + endIndex) >>> 1;
-				if(startIndex==i) {
+				if (target >= zeroColumn[i] && target <= lastColumn[i]) {
+					answer = i;
 					break;
-				}else if(target >= zeroColumn[i]) {
-					startIndex = i+1;
+				} else if (target > lastColumn[i]) {
+					startIndex = i + 1;
 				} else {
 					endIndex = i - 1;
-					}
 				}
-		answer=startIndex;		
-		System.out.println("search in row "+answer);
-		return answer;
 			}
+			return answer;
+		}
 	}
 }
